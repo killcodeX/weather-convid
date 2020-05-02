0f89bf326371cc8ec744a21d00d0d5e5d55bb2bd
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-05-02 16:33:23 +0530

    added function to input

--- my-app/src/components/login/register.js
+++ my-app/src/components/login/register.js
@@ -1,82 +1,93 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './register.css';
 
-class Register extends Component {
-    register(){
-        alert('login called');
+function Register (props) {
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [cpassword, setCpassword] = useState();
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+        alert(`Submitting Name ${name}`);
+        alert(`Submitting Email ${email}`);
+        alert(`Submitting Passowrd ${password}`);
+        alert(`Submitting Cpassword ${cpassword}`);
     }
-    render() {
-        return (
-            <div>
-                <section className="section" id="register">
-                    <div className="container">
-                        <div className="box">
-                            <div className="columns">
-                                <div className="column">
-                                <h1 className="title"> Sign Up</h1>
-                                    <form>
-                                        <div className="field">
-                                            <p className="control has-icons-left">
-                                                <input className="input" type="text" placeholder="Your Name"/>
-                                                <span className="icon is-small is-left">
-                                                    <i class="fas fa-user"></i>
-                                                </span>
-                                            </p>
+
+    return (
+        <div>
+            <section className="section" id="register">
+                <div className="container">
+                    <div className="box">
+                        <div className="columns">
+                            <div className="column">
+                            <h1 className="title"> Sign Up</h1>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="field">
+                                        <p className="control has-icons-left">
+                                            <input className="input" type="text" placeholder="Your Name"
+                                            value={name} onChange={e => setName(e.target.value)}/>
+                                            <span className="icon is-small is-left">
+                                                <i class="fas fa-user"></i>
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div className="field">
+                                        <p className="control has-icons-left">
+                                            <input className="input" type="text" placeholder="Your Email"
+                                            value={email} onChange={e => setEmail(e.target.value)}/>
+                                            <span className="icon is-small is-left">
+                                                <i class="fas fa-envelope"></i>
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div className="field">
+                                        <p className="control has-icons-left">
+                                            <input className="input" type="password" placeholder="Password"
+                                            value={password} onChange={e => setPassword(e.target.value)}/>
+                                            <span className="icon is-small is-left">
+                                                <i class="fas fa-lock"></i>
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div className="field">
+                                        <p className="control has-icons-left">
+                                            <input className="input" type="password" placeholder="Repeat your Password"
+                                            value={cpassword} onChange={e => setCpassword(e.target.value)}/>
+                                            <span className="icon is-small is-left">
+                                                <i class="fas fa-lock"></i>
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div className="field">
+                                        <div className="control">
+                                            <label className="checkbox">
+                                                <input type="checkbox"/> I agree all the statements in <a href="#">Terms of service</a>
+                                            </label>
                                         </div>
-                                        <div className="field">
-                                            <p className="control has-icons-left">
-                                                <input className="input" type="text" placeholder="Your Email"/>
-                                                <span className="icon is-small is-left">
-                                                    <i class="fas fa-envelope"></i>
-                                                </span>
-                                            </p>
+                                    </div>
+                                    <div className="field">
+                                        <div className="control">
+                                            <button className="button is-link" type='submit'>Register</button>
                                         </div>
-                                        <div className="field">
-                                            <p className="control has-icons-left">
-                                                <input className="input" type="password" placeholder="Password"/>
-                                                <span className="icon is-small is-left">
-                                                    <i class="fas fa-lock"></i>
-                                                </span>
-                                            </p>
-                                        </div>
-                                        <div className="field">
-                                            <p className="control has-icons-left">
-                                                <input className="input" type="password" placeholder="Repeat your Password"/>
-                                                <span className="icon is-small is-left">
-                                                    <i class="fas fa-lock"></i>
-                                                </span>
-                                            </p>
-                                        </div>
-                                        <div className="field">
-                                            <div className="control">
-                                                <label className="checkbox">
-                                                    <input type="checkbox"/> I agree all the statements in <a href="#">Terms of service</a>
-                                                </label>
-                                            </div>
-                                        </div>
-                                        <div className="field">
-                                            <div className="control">
-                                                <button className="button is-link" onClick={()=>this.register()}>Register</button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                                <div className="column">
-                                    <img className="reg-img" src={require('./signup-image.webp')}/>
-                                    <a  className="reg" href="#">I am already a member</a>
-                                </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="column">
+                                <img className="reg-img" src={require('./signup-image.webp')}/>
+                                <a  className="reg" href="#">I am already a member</a>
                             </div>
                         </div>
                     </div>
-                </section>
-            </div>
-        )
-    }
+                </div>
+            </section>
+        </div>
+    )
 }
 
-Register.propTypes = {
-    name: PropTypes.string
-};
+
 
 export default Register;
