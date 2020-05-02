2d3e7f14a34857cb607522c085e52038a3e713f6
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-05-02 21:20:34 +0530

    added authentication

--- my-app/src/components/login/register.js
+++ my-app/src/components/login/register.js
@@ -1,8 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './register.css';
 
-function Register (props) {
+// dtatabase api
+// const SPREADHEET_ID = '1j8CwAzsUhVYNM3C1f3C1NZhV3JFot22FdU-Qkvo_hG0';
+// const CLIENT_ID =  '828401526522-tgtqqhkcvbkm87inge2kbt93stl3ro5e.apps.googleusercontent.com';
+// const API_KEY = 'AIzaSyDyRXFUYL5LGp0zdxfIAZ9QxVV643yhmUI';
+// const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
 
+function Register () {
+
+    // for api
+    // useEffect(() => {
+    //     // code to run on component mount
+    //     window.gapi.load('client:auth2', initClient); 
+    //   }, [])
+
+
+    //   initClient =()=> { //provide the authentication credentials you set up in the Google developer console
+    //     gapi.client.init({
+    //       'apiKey': API_KEY,
+    //       'clientId': CLIENT_ID,
+    //       'scope': SCOPE,
+    //       'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    //     }).then(()=> {
+    //       gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus); //add a function called `updateSignInStatus` if you want to do something once a user is logged in with Google
+    //       updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    //     });
+    //   }
+    // hooks for validation
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -15,14 +40,12 @@ function Register (props) {
             alert('please enter your name');
             return bool;
         }
-        else if(email == ''){
-            if(typeof email !== "undefined"){
+        else if((email == '') || (typeof email !== "undefined")){
                 let lastAtPos = email.lastIndexOf('@');
                 let lastDotPos = email.lastIndexOf('.');
      
                 if (!(lastAtPos < lastDotPos && lastAtPos > 0 && email.indexOf('@@') == -1 && lastDotPos > 2 && (email.length - lastDotPos) > 2)) {
                    alert("Email is not valid");
-                 }
             }  
         }
         else if (password == ''){
@@ -41,7 +64,28 @@ function Register (props) {
     const handleSubmit =(e) =>{
         e.preventDefault();
         if(validateForm() == false){
-            alert('this works');
+            alert('this not works');
+        }
+        else{
+            let data = {
+                name: name,
+                email: email,
+                password: password,
+                cpassword: cpassword
+            };
+            // alert(JSON.stringify(data));
+            fetch("https://sheetsu.com/apis/v1.0su/ac25abd2d8c3", {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify(data)
+                }).then( (response) => {
+                    return response.json()
+                }).then( (json) => {
+                    console.log(json);
+                });
         }
     }
 
